1b91ca83148666a63dcc24acd129abefcdbd11d9
Will Cauthen <willcauthen50@gmail.com>
2016-06-17 12:57:39 -0700

    test for chef class

--- spec/models/chef_spec.rb
+++ spec/models/chef_spec.rb
@@ -3,9 +3,14 @@ require 'rails_helper'
 RSpec.describe Chef, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
   it 'should exist' do
-  # describe (Chef).to_not be_nil
-  	test = Chef.new(name: "Taco")
-  	p "the test object", test
-  	expect(test.name).to eq("Taco")
+  	examp = Chef.new(name: "David", description: "I am a test. I am only a test", position: "Cook").save
+  	p "the test object", examp
+  	expect(examp.name).to eq("David")
+  	expect(examp.position).to eq('Cook')
+  	expect(examp.description).to include('I am a test')
+  	expect(examp.image_url).to be_nil
   end
+
+  # it 'should save to database' do
+  # end
 end
