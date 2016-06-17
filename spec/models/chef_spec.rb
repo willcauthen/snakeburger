d4df0e30c5f4abe90d6b9e68a6612a17a48669f7
Will Cauthen <willcauthen50@gmail.com>
2016-06-17 13:27:04 -0700

    rspec tests for Chef class, creates new instance in database, tests it, and deletes it

--- spec/models/chef_spec.rb
+++ spec/models/chef_spec.rb
@@ -2,15 +2,17 @@ require 'rails_helper'
 
 RSpec.describe Chef, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
-  it 'should exist' do
-  	examp = Chef.new(name: "David", description: "I am a test. I am only a test", position: "Cook").save
-  	p "the test object", examp
+  it 'should exist, save to database, and be deleted from database' do
+  	examp = Chef.new(name: "David", description: "I am a test. I am only a test", position: "Cook")
+  	examp.save
   	expect(examp.name).to eq("David")
   	expect(examp.position).to eq('Cook')
   	expect(examp.description).to include('I am a test')
   	expect(examp.image_url).to be_nil
+		### 
+  	expect(Chef.find_by name: 'David').to_not be_nil
+  	target = Chef.find_by name: 'David'
+  	target.delete
+  	expect(Chef.find_by name: 'David').to be_nil
   end
-
-  # it 'should save to database' do
-  # end
 end
